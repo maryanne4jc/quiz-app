2d2571e23a171fa8436ce96c91e5abc52307754a
Mary Anne <maryanne4jc@gmail.com>
2024-03-26 20:54:07 +0300

    commit

---
+++ questions.js
@@ -0,0 +1,66 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is photosynthesis?",
+        answer: "C: The process by which plants make their own food",
+        options: [
+            "A. Soil erosion",
+            "B. The process by which children grow",
+            "C: The process by which plants make their own food",
+            "D: I don't know"
+
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Which of the following is a mammal?",
+        answer: "A: Dolphin",
+        options: [
+            "A: Dolphin",
+            "B: Shark",
+            "C: Crocodile",
+            "D: Octopus"
+
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What is the closest planet to the Sun?",
+        answer: "B: Mercury",
+        options: [
+            "A: Earth",
+            "B: Mercury",
+            "C: Mars",
+            "D: Venus"
+
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What is the chemical symbol for water?",
+        answer: "D: H2O",
+        options: [
+            "A: CO2",
+            "B: NaCl",
+            "C: O2",
+            "D: H2O"
+
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What is the process by which plants release oxygen into the air?",
+        answer: "A: Photosynthesis",
+        options: [
+            "A: Photosynthesis",
+            "B: Respiration",
+            "C: Transpiration",
+            "D: Combustion"
+
+        ]
+    }
+]
